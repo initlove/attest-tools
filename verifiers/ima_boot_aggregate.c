3a4c8e250fde7661257aba022d677bf0af5399da
Roberto Sassu <roberto.sassu@huawei.com>
2020-01-27 10:52:20 +0100

    Add support for non-SHA1 boot aggregate

--- verifiers/ima_boot_aggregate.c
+++ verifiers/ima_boot_aggregate.c
@@ -23,7 +23,7 @@
 
 int verify(attest_ctx_data *d_ctx, attest_ctx_verifier *v_ctx)
 {
-	unsigned char buffer[IMPLEMENTATION_PCR * SHA_DIGEST_LENGTH];
+	unsigned char buffer[IMPLEMENTATION_PCR * SHA512_DIGEST_SIZE];
 	unsigned char *buffer_ptr = buffer;
 	const unsigned char *digest_ptr;
 	const char *algo_ptr;
@@ -39,24 +39,6 @@ int verify(attest_ctx_data *d_ctx, attest_ctx_verifier *v_ctx)
 
 	log = attest_ctx_verifier_add_log(v_ctx, "verify IMA boot aggregate");
 
-	for (i = 0; i < 8; i++) {
-		pcr = attest_pcr_get(v_ctx, i, TPM_ALG_SHA1);
-		if (!pcr)
-			goto out;
-
-		rc = TSS_Array_Marshal((uint8_t *)&pcr->digest,
-				       SHA_DIGEST_LENGTH, &written,
-				       &buffer_ptr, &size);
-		check_goto(rc, -EINVAL, out, v_ctx,
-			   "TSS_Array_Marshal() error: %d", rc);
-	}
-
-	digest.hashAlg = TPM_ALG_SHA1;
-
-	rc = TSS_Hash_Generate(&digest, written, buffer, 0, NULL);
-	check_goto(rc, -EINVAL, out, v_ctx,
-		   "TSS_Hash_Generate() error: %d", rc);
-
 	ima_log = attest_event_log_get(v_ctx, "ima");
 	check_goto(!ima_log, -ENOENT, out, v_ctx,
 		   "IMA event log not provided");
@@ -69,13 +51,29 @@ int verify(attest_ctx_data *d_ctx, attest_ctx_verifier *v_ctx)
 				     &digest_len, &digest_ptr);
 	check_goto(rc, rc, out, v_ctx, "event data digest not found");
 
-	check_goto(algo_len != 4 || strncmp(algo_ptr, "sha1", algo_len),
-		   -ENOTSUP, out, v_ctx,
-		   "expected SHA1 algorithm for boot aggregate");
+	digest.hashAlg = attest_pcr_bank_alg_from_name((char *)algo_ptr,
+						       algo_len);
+
+	for (i = 0; i < 8; i++) {
+		pcr = attest_pcr_get(v_ctx, i, digest.hashAlg);
+		if (!pcr)
+			goto out;
+
+		rc = TSS_Array_Marshal((uint8_t *)&pcr->digest,
+				       TSS_GetDigestSize(digest.hashAlg),
+				       &written, &buffer_ptr, &size);
+		check_goto(rc, -EINVAL, out, v_ctx,
+			   "TSS_Array_Marshal() error: %d", rc);
+	}
+
+	rc = TSS_Hash_Generate(&digest, written, buffer, 0, NULL);
+	check_goto(rc, -EINVAL, out, v_ctx,
+		   "TSS_Hash_Generate() error: %d", rc);
 
 	boot_aggregate_entry->flags |= LOG_ENTRY_PROCESSED;
 
-	rc = memcmp((uint8_t *)&digest.digest, digest_ptr, SHA_DIGEST_LENGTH);
+	rc = memcmp((uint8_t *)&digest.digest, digest_ptr,
+		    TSS_GetDigestSize(digest.hashAlg));
 	check_goto(rc, rc, out, v_ctx, "calculated digest != provided digest");
 out:
 	attest_ctx_verifier_end_log(v_ctx, log, rc);
